71d6a9a1632fae582e0977b520d5a2944dc98f0e
Fairuzajs <yaubasarova.93@mail.ru>
2023-05-02 00:49:10 +0500

    feat: add eslint

--- js.js
+++ js.js
@@ -9,6 +9,9 @@ let keyboard = document.createElement("div");
 body.appendChild(keyboard);
 keyboard.id = "keyboard";
 
+//let key = document.createElement("button");
+//key.classList.add("key");
+//keyboard.appendChild(key);
 
 keyboard = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8, 9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 46, 20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13, 16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38, 16, 17, 91, 18, 32, 18, 17, 37, 40, 39];
 
@@ -29,7 +32,7 @@ function init() {
     if (i == 14 || i == 28 || i == 41 || i == 54) {
       out += '<div class="line-break"></div>';
     }
-    out += '<div class="key" data="' +keyboard[i]+ '">' + String.fromCharCode(keyboard[i]) + '</div>';
+    out += '<div class="key" data="' + keyboard[i] + '">' + String.fromCharCode(keyboard[i]) + '</div>';
   }
   document.querySelector('#keyboard').innerHTML = out;
 }
@@ -48,7 +51,7 @@ document.querySelectorAll('#keyboard .key').forEach(function(element) {
 
 document.querySelectorAll('#keyboard .key').forEach(function(element) {
 
-  element.onclick = function ( event) {
+  element.onclick = function (event) {
     document.querySelectorAll('#keyboard .key').forEach(function (element) {
       element.classList.remove('active');
    });
